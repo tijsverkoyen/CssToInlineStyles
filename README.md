3854223540a58b4eab3e17ba1a2d6644a1d5b907
Tijs Verkoyen <tijs@verkoyen.eu>
2010-12-05 15:30:06 +0100

    Added README

---
+++ README.md
@@ -0,0 +1,21 @@
+# CssToInlineStyles class
+
+> CssToInlineStyles is a class that enables you to convert HTML-pages/files into HTML-pages/files with inline styles. This is very usefull when you're sending emails.
+
+## About
+
+PHP CssToInlineStyles is a class to convert HTML into HTML with inline styles.
+
+## License
+
+PHP CssToInlineStyles is [BSD](http://classes.verkoyen.eu/overview/bsd) licensed.
+
+## Documentation
+
+The class is well documented inline. If you use a decent IDE you'll see that each method is documented with PHPDoc.
+More documentation can be found on [http://classes.verkoyen.eu/css_to_inline_styles/docs](http://classes.verkoyen.eu/css_to_inline_styles/docs).
+
+## Sites using this class
+
+* [Each site based on Fork CMS](http://www.fork-cms.com)
+* [Print en Bind](http://www.printenbind.nl)
--- css_to_inline_styles.php
+++ css_to_inline_styles.php
@@ -37,7 +37,7 @@
  * This software is provided by the author "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. In no event shall the author be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
  *
  * @author		Tijs Verkoyen <php-css-to-inline-styles@verkoyen.eu>
- * @version		1.0.2
+ * @version		1.0.3
  *
  * @copyright	Copyright (c) 2010, Tijs Verkoyen. All rights reserved.
  * @license		BSD License
