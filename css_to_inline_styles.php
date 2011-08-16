1a4902e99e1a9da7637d10bb50e8525d462a5f7a
Tijs Verkoyen <tijs@verkoyen.eu>
2011-08-16 11:09:52 +0200

    Bumped version

--- css_to_inline_styles.php
+++ css_to_inline_styles.php
@@ -11,11 +11,9 @@
  * The class is documented in the file itself. If you find any bugs help me out and report them. Reporting can be done by sending an email to php-css-to-inline-styles-bugs[at]verkoyen[dot]eu.
  * If you report a bug, make sure you give me enough information (include your code).
  *
- * Changelog since 1.0.5
- * - preserve original styles.
- *
  * Changelog since 1.0.4
  * - beter handling of XHTML output, thx to Michele Locati.
+ * - preserve original styles.
  *
  * Changelog since 1.0.3
  * - fixed some code-styling issues
@@ -47,7 +45,7 @@
  * This software is provided by the author "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. In no event shall the author be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
  *
  * @author		Tijs Verkoyen <php-css-to-inline-styles@verkoyen.eu>
- * @version		1.0.4
+ * @version		1.0.5
  *
  * @copyright	Copyright (c), Tijs Verkoyen. All rights reserved.
  * @license		BSD License
