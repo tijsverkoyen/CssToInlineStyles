f9a407a536ae0e14b1b33f256100e2b7ab89d71f
John Boehr <jbboehr@gmail.com>
2014-07-19 19:42:50 -0700

    added composer autoloader to test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,7 @@
 $basepath = realpath(dirname(dirname(__FILE__)));
 $testpath = realpath(dirname(__FILE__));
 
+require $basepath . '/vendor/autoload.php';
 require $basepath . '/CssToInlineStyles.php';
 require $basepath . '/Exception.php';
 
