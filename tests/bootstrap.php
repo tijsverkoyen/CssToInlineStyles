18fe720bd6b54e4fb2a3a11e1cfcf71764caa3b9
John Boehr <jbboehr@gmail.com>
2014-07-22 16:05:28 -0700

    simplify test bootstrap

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$basepath = realpath(dirname(dirname(__FILE__)));
-$testpath = realpath(dirname(__FILE__));
-
-require $basepath . '/vendor/autoload.php';
-require $basepath . '/CssToInlineStyles.php';
-require $basepath . '/Exception.php';
-
