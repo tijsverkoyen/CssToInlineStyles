59262efe0e55c42ef9a7524c5f5f8377dbfd2101
Barry vd. Heuvel <barry@fruitcakestudio.nl>
2015-10-08 12:42:44 +0200

    Update to support Symfony 3.x

--- src/Selector.php
+++ src/Selector.php
@@ -2,6 +2,7 @@
 namespace TijsVerkoyen\CssToInlineStyles;
 
 use Symfony\Component\CssSelector\CssSelector;
+use Symfony\Component\CssSelector\CssSelectorConverter;
 use Symfony\Component\CssSelector\Exception\ExceptionInterface;
 
 /**
@@ -17,6 +18,9 @@ class Selector
      */
     protected $selector;
     
+    /**
+     * @param  string $selector The CSS selector
+     */
     public function __construct($selector)
     {
         $this->selector = $selector;
@@ -25,7 +29,12 @@ class Selector
     public function toXPath()
     {
         try {
-            $query = CssSelector::toXPath($this->selector);
+            if (class_exists('Symfony\Component\CssSelector\CssSelectorConverter')) {
+                $converter = new CssSelectorConverter();
+                $query = $converter->toXPath($this->selector);
+            } else {
+                $query = CssSelector::toXPath($this->selector);
+            }
         } catch (ExceptionInterface $e) {
             $query = null;
         }
