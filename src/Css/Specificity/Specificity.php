b09df2a88ac4c86d577827bf223d20749b68acb7
Tijs Verkoyen <tijs@verkoyen.eu>
2015-12-08 15:35:34 +0100

    Extend Symfony\Component\CssSelector\Node\Specificity

--- src/Css/Specificity/Specificity.php
+++ src/Css/Specificity/Specificity.php
@@ -2,86 +2,8 @@
 
 namespace TijsVerkoyen\CssToInlineStyles\Css\Specificity;
 
-class Specificity
+class Specificity extends \Symfony\Component\CssSelector\Node\Specificity
 {
-    const A_FACTOR = 100;
-    const B_FACTOR = 10;
-    const C_FACTOR = 1;
-
-    /**
-     * @var int
-     */
-    private $a;
-
-    /**
-     * @var int
-     */
-    private $b;
-
-    /**
-     * @var int
-     */
-    private $c;
-
-    /**
-     * Constructor.
-     *
-     * @param int $a
-     * @param int $b
-     * @param int $c
-     */
-    public function __construct($a, $b, $c)
-    {
-        $this->a = $a;
-        $this->b = $b;
-        $this->c = $c;
-    }
-
-    /**
-     * @param Specificity $specificity
-     *
-     * @return Specificity
-     */
-    public function plus(Specificity $specificity)
-    {
-        return new self($this->a + $specificity->a, $this->b + $specificity->b, $this->c + $specificity->c);
-    }
-
-    /**
-     * Returns global specificity value.
-     *
-     * @return int
-     */
-    public function getValue()
-    {
-        return $this->a * self::A_FACTOR + $this->b * self::B_FACTOR + $this->c * self::C_FACTOR;
-    }
-
-    /**
-     * Returns -1 if the object specificity is lower than the argument,
-     * 0 if they are equal, and 1 if the argument is lower.
-     *
-     * @param Specificity $specificity
-     *
-     * @return int
-     */
-    public function compareTo(Specificity $specificity)
-    {
-        if ($this->a !== $specificity->a) {
-            return $this->a > $specificity->a ? 1 : -1;
-        }
-
-        if ($this->b !== $specificity->b) {
-            return $this->b > $specificity->b ? 1 : -1;
-        }
-
-        if ($this->c !== $specificity->c) {
-            return $this->c > $specificity->c ? 1 : -1;
-        }
-
-        return 0;
-    }
-
     /**
      * Calculate the specificity based on a CSS Selector string,
      * Based on the patterns from premailer/css_parser by Alex Dunae
